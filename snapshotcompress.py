fc2c0be26d0d0b9160992ec40c7a2f622fe03ce6
dddsnn <marc.lehmann@gmx.de>
2015-03-27 21:07:46 +0100

    compression works (i think) still lots of todos

--- snapshotcompress.py
+++ snapshotcompress.py
@@ -114,26 +114,35 @@ class DeltaCube(Cube):
     def values(self):
         return self._values
 
-    def compressed(self, position_config, orientation_config):
-        res = bs.BitStream()
-        # largest orientation index
-        try:
-            res += bs.Bits(int=self.orientation_largest, length=2)
-        except CreationError:
-            res += bs.Bits(uint=self.orientation_largest, length=2)
-        # orientation
-        for x in (self.orientation_a, self.orientation_b, self.orientation_c):
-                res += encode_varint(x, orientation_config)
-        # position
-        for x in (self.position_x, self.position_y, self.position_z):
-            res += encode_varint(x, position_config)
-        # interacting
-        res += bs.Bits(uint=self.interacting, length=1)
-        return res
+class Configs:
+    def __init__(self, position_config, orientation_config):
+        self.pos = position_config
+        self.ortn = orientation_config
 
-def encode_varint(n, configs):
+def compress_delta_cube(cube, configs):
+    res = bs.BitStream()
+    # largest orientation index
+    try:
+        res += bs.Bits(int=cube.orientation_largest, length=2)
+    except CreationError:
+        res += bs.Bits(uint=cube.orientation_largest, length=2)
+    # orientation
+    for x in (cube.orientation_a, cube.orientation_b, cube.orientation_c):
+        res += encode_varint(x, configs.ortn, 9)
+    # position x,y
+    for x in (cube.position_x, cube.position_y):
+        res += encode_varint(x, configs.pos, 18)
+    # position z
+    res += encode_varint(cube.position_z, configs.pos, 14)
+    # interacting
+    res += bs.Bits(uint=cube.interacting, length=1)
+    return res
+
+def encode_varint(n, config, max_len):
+    # TODO remove overlap
+    # TODO uint version
     last = False
-    for i, length in enumerate(configs[:-1]):
+    for i, length in enumerate(config):
         try:
             bits = bs.Bits(int=n, length=length)
             config_idx = i
@@ -142,18 +151,44 @@ def encode_varint(n, configs):
             continue
     else:
         last = True
-        config_idx = len(configs) - 1
+        config_idx = len(config)
         try:
-            bits = bs.Bits(int=n, length=configs[-1])
+            bits = bs.Bits(int=n, length=max_len)
         except CreationError:
-            bits = bs.Bits(uint=n, length=configs[-1])
+            bits = bs.Bits(uint=n, length=max_len)
     header = bs.BitStream()
     # write config_idx 1s
-    header += bs.pack('{0}*bool'.format(config_idx), itertools.repeat(True))
+    header += bs.pack('{0}*bool'.format(config_idx),
+                      *itertools.repeat(True, config_idx))
     # if we're not using the last config, write a 0 to terminate
     if not last:
         header += bs.Bits(bool=False)
     return header + bits
+
+def compress_delta_frame(baseline_frame, current_frame):
+    res = bs.BitStream()
+    deltas = [DeltaCube(baseline, current)
+              for baseline, current in zip(baseline_frame, current_frame)]
+    changed_deltas = [(i, d) for (i, d) in enumerate(deltas) if any(d)]
+    num_configs = 1
+    configs = Configs([5, 9], [5, 8])
+    index_list_config = [3, 5]
+    # write num of configs (zero unnecessary, always at least 1)
+    res += bs.Bits(ue=num_configs - 1)
+    # TODO write configs (num of settings and settings, for pos and ortn, last setting implicit (num of bits for the whole thing))
+    # TODO make the following a list
+    # for every config, the number of cubes encoded with it in the index list
+    # can be 0 for one, then a changed bit setup is used, otherwise unchanged
+    # cubes can simply be omitted
+    num_in_index_list = len(changed_deltas)
+    res += bs.Bits(ue=num_in_index_list)
+    # TODO use uint version of varint
+    res += encode_varint(changed_deltas[0][0], index_list_config, 10)
+    res += compress_delta_cube(changed_deltas[0][1], configs)
+    for b, c in zip(changed_deltas[:-1], changed_deltas[1:]):
+        res += encode_varint(c[0] - b[0], index_list_config, 10)
+        res += compress_delta_cube(c[1], configs)
+    return res
 
 def rl_enc(bits):
     def write_zeros():
@@ -178,22 +213,13 @@ def rl_enc(bits):
         write_zeros()
     return res
 
-def compress_delta_frame(baseline_frame, current_frame):
-    res = bs.BitStream()
-    deltas = [DeltaCube(baseline, current)
-              for baseline, current in zip(baseline_frame, current_frame)]
-    num_changed = sum(1 for d in deltas if any(d))
-    print(num_changed)
-#     print(min(d.orientation_a for d in deltas))
-
 if __name__ == '__main__':
     filename = '/home/dddsnn/Downloads/delta_data.bin'
     data = Data(filename)
-#     frame = data[100]
 #     print(sum(1 for frame in data[6:20] for cube in frame if not cube.position_x))
 #     print(min(cube.position_z for frame in data for cube in frame))
-    for baseline_frame, current_frame in zip(data[100:110], data[106:116]):
-        compress_delta_frame(baseline_frame, current_frame)
+    for baseline_frame, current_frame in zip(data[2000:2010], data[2006:2016]):
+        print(len(compress_delta_frame(baseline_frame, current_frame)))
 #         for baseline, current in zip(baseline_frame, current_frame):
 #             delta = DeltaCube(baseline, current)
 #             print(delta.position_x)
